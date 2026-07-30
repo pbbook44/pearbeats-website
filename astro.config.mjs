56b5475c259e89e188441a6d0a971f2b4e3b7642
pbbook44 <admin@pearbeats.de>
2026-07-30 17:35:07 +0200

    Update paths for pearbeats.de domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://pbbook44.github.io/pearbeats-website/',
+  site: 'https://pearbeats.de',
   output: 'static',
-  base: '/pearbeats-website/',
   outDir: 'docs'
 });
